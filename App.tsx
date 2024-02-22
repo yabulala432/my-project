3208783c1a4466caaf13d042000a4828a10e3c96
Yeabsira Yonas <yabulala432@gmail.com>
2024-02-23 02:32:52 +0300

    beautify

--- App.tsx
+++ App.tsx
@@ -1,22 +1,13 @@
-import StackNavLogin from "./app/navigation/StackNavLogin";
-import HomeScreen from "./app/screens/HomeScreen";
 import React from "react";
-import SignIn from "./app/screens/signinAndRegister/SignIn";
-import SignUp from "./app/screens/signinAndRegister/SignUp";
-import ForgotPassword from "./app/screens/signinAndRegister/ForgotPassword";
+
 import { NavigationContainer } from "@react-navigation/native";
-import Route from "./app/screens/Route";
-import GpsTracker from "./app/screens/GpsTracker";
-import OnboardingScreen from "./app/screens/onboardingScreen/OnboardingScreen";
-import BottomTabNavigator from "./app/navigation/Animated/navigator/BottomTabNavigator";
+
+import StackNavLogin from "./app/navigation/StackNavLogin";
 
 export default function App() {
   return (
     <NavigationContainer>
       <StackNavLogin />
-      {/* <BottomTabNavigator /> */}
     </NavigationContainer>
   );
 }
-
-//
